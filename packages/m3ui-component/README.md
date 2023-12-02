ccfe92885341601fb16829b80934917a7b1bcefd
Taeyeob Yeo <id1423fo@gmail.com>
2023-12-02 13:32:48 +0900

    feat: component pkg setup

---
+++ packages/m3ui-component/README.md
@@ -0,0 +1,11 @@
+# `m3ui-component`
+
+> TODO: description
+
+## Usage
+
+```
+const m3uiComponent = require('m3ui-component');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/m3ui-component/__tests__/m3ui-component.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const m3uiComponent = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(m3uiComponent(), 'Hello from m3uiComponent');
+console.info('m3uiComponent tests passed');
