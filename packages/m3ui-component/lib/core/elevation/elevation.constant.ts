87140d35dc2addcf72af4f751757c44301934f25
Taeyeob Yeo <id1423fo@gmail.com>
2023-12-03 16:55:34 +0900

    fix: elevation level fixed to 0-5

--- packages/m3ui-component/lib/core/elevation/elevation.constant.ts
+++ packages/m3ui-component/lib/core/elevation/elevation.constant.ts
@@ -1,5 +1,10 @@
 import { CSSProperties } from "react";
 
 export type Elevation = {
-  [level: number]: CSSProperties["boxShadow"];
+  0: CSSProperties["boxShadow"]; // surface
+  1: CSSProperties["boxShadow"]; // surface-container-low
+  2: CSSProperties["boxShadow"]; // surface-container
+  3: CSSProperties["boxShadow"]; // surface-container-high
+  4: CSSProperties["boxShadow"]; // surface-container-high
+  5: CSSProperties["boxShadow"]; // surface-container-highest
 };
