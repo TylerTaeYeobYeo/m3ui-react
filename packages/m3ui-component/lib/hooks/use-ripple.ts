471383fac239dd839323747fa5c86c49a7206cd6
Taeyeob Yeo <id1423fo@gmail.com>
2023-12-04 19:42:11 +0900

    fix: ripple origin move on mouse move

--- packages/m3ui-component/lib/hooks/use-ripple.ts
+++ packages/m3ui-component/lib/hooks/use-ripple.ts
@@ -1,5 +1,65 @@
 import { MutableRefObject, useEffect } from "react";
-import { rippleEventFactory } from "../utils/style.util";
+
+/**
+ * @description
+ * bind to mousedown event
+ */
+export const rippleEventFactory = (
+  element: HTMLElement,
+  classNamePrefix: string = ""
+) => {
+  const onMouseUpFactory = (ripple: HTMLSpanElement, callback: () => void) => {
+    const onMouseUp = () => {
+      callback();
+      element.removeEventListener("mouseup", onMouseUp);
+      element.removeEventListener("mouseleave", onMouseUp);
+      ripple.classList.add(`${classNamePrefix}ripple-fade-out`);
+      ripple.addEventListener("transitionend", () => {
+        element.removeChild(ripple);
+      });
+    };
+    return onMouseUp;
+  };
+
+  return (e) => {
+    e.stopPropagation();
+    const ripple = document.createElement("span");
+    ripple.classList.add(`${classNamePrefix}ripple`);
+
+    const x = e.clientX - element.offsetLeft;
+    const y = e.clientY - element.offsetTop;
+
+    const size = Math.max(element.clientWidth, element.clientHeight);
+    const half = size / 2;
+    const quarter = half / 2;
+
+    ripple.style.width = `${half}px`;
+    ripple.style.height = `${half}px`;
+    ripple.style.marginTop = `-${quarter}px`;
+    ripple.style.marginLeft = `-${quarter}px`;
+
+    ripple.style.left = `${x}px`;
+    ripple.style.top = `${y}px`;
+    element.appendChild(ripple);
+
+    // const mousemove = (e) => {
+    //   const x = e.clientX - element.offsetLeft;
+    //   const y = e.clientY - element.offsetTop;
+    //   ripple.style.left = `${x}px`;
+    //   ripple.style.top = `${y}px`;
+    // };
+    // element.addEventListener("mousemove", mousemove);
+    const mouseUp = onMouseUpFactory(ripple, () => {
+      // element.removeEventListener("mousemove", mousemove);
+    });
+    element.addEventListener("mouseup", mouseUp);
+    element.addEventListener("mouseleave", mouseUp);
+
+    setTimeout(() => {
+      ripple.classList.add(`${classNamePrefix}ripple-scale`);
+    }, 1);
+  };
+};
 
 /**
  * @description use ripple effect on HtmlElement
