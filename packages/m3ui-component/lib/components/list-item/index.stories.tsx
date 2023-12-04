07206e8506eb075ca204772d3f99642ccea6a84d
Taeyeob Yeo <id1423fo@gmail.com>
2023-12-04 19:23:04 +0900

    feat: list item added

---
+++ packages/m3ui-component/lib/components/list-item/index.stories.tsx
@@ -0,0 +1,55 @@
+import { action } from "@storybook/addon-actions";
+import type { Meta, StoryObj } from "@storybook/react";
+import { LIST_ITEM_SHAPE, ListItem } from ".";
+import { Icon } from "../icon";
+
+const meta: Meta<typeof ListItem> = {
+  component: ListItem,
+  tags: ["autodocs"],
+  argTypes: {},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ListItem>;
+
+export const Default: Story = {
+  args: {
+    leading: <Icon icon="man" />,
+    headline: "Headline",
+    children: "Supporting Text",
+    trailing: <Icon icon="wifi" />,
+  },
+  render: (props) => <ListItem {...props} />,
+};
+
+export const Video: Story = {
+  args: {
+    shape: LIST_ITEM_SHAPE.VIDEO,
+    leading: (
+      <video
+        src="http://www.w3schools.com/html/mov_bbb.mp4"
+        loop
+        muted
+        autoPlay
+        width="100%"
+        height="100%"
+      />
+    ),
+    headline: "Headline",
+    children: "Supporting Text",
+    trailing: <Icon icon="wifi" />,
+  },
+  render: (props) => <ListItem {...props} />,
+};
+
+export const ClickEvent: Story = {
+  args: {
+    leading: <Icon icon="man" />,
+    headline: "Headline",
+    children: "Supporting Text",
+    trailing: <Icon icon="wifi" />,
+    onClick: action("onClick"),
+  },
+  render: (props) => <ListItem {...props} />,
+};
---
+++ packages/m3ui-component/lib/components/list-item/index.tsx
@@ -0,0 +1,170 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+import { HTMLAttributes, ReactNode, forwardRef, useRef } from "react";
+import { useTheme } from "../../core/theme-provider/hook";
+import { useRipple } from "../../hooks/use-ripple";
+import { mixColor } from "../../utils/style.util";
+
+export enum LIST_ITEM_SHAPE {
+  DEFAULT = "default",
+  VIDEO = "video",
+}
+
+/**
+ * children is the supporting text of the list Item
+ */
+export type ListItemProps = {
+  leading?: ReactNode;
+  leadingWrapperProps?: HTMLAttributes<HTMLDivElement>;
+
+  headline: ReactNode;
+  headlineWrapperProps?: HTMLAttributes<HTMLDivElement>;
+
+  trailing?: ReactNode;
+  trailingWrapperProps?: HTMLAttributes<HTMLDivElement>;
+
+  contentWrapperProps?: HTMLAttributes<HTMLDivElement>;
+  childrenWrapperProps?: HTMLAttributes<HTMLDivElement>;
+
+  bottomDivider?: boolean;
+  shape?: LIST_ITEM_SHAPE;
+  rippleEffect?: boolean;
+} & HTMLAttributes<HTMLLIElement>;
+
+const BaseListItem = styled.li<{
+  classNamePrefix: string;
+  bottomDivider?: boolean;
+  shape: LIST_ITEM_SHAPE;
+}>`
+  position: relative;
+  overflow: hidden;
+  display: flex;
+  gap: 16px;
+  padding: ${({ shape }) =>
+    shape === LIST_ITEM_SHAPE.VIDEO ? "12px 16px 12px 0px" : "8px 16px"};
+  .${({ classNamePrefix }) => classNamePrefix}icon {
+    width: 24px;
+    height: 24px;
+    font-size: 24px;
+  }
+  ${({ onClick }) =>
+    onClick
+      ? css`
+          cursor: pointer;
+          transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
+          &:focus-visible {
+            background-color: ${mixColor(
+              "var(--onSurface)",
+              "transparent",
+              12
+            )};
+          }
+          &:hover {
+            background-color: ${mixColor("var(--onSurface)", "transparent", 8)};
+          }
+          &:active {
+            background-color: ${mixColor(
+              "var(--onSurface)",
+              "transparent",
+              12
+            )};
+          }
+        `
+      : ""}
+  &:-webkit-user-drag,
+  &:-moz-window-dragging,
+  &:has([data-dragging="true"]) {
+    background-color: ${mixColor("var(--onSurface)", "transparent", 16)};
+    box-shadow: var(--elevation-3);
+  }
+`;
+
+const LeadingContent = styled.div<{ shape: LIST_ITEM_SHAPE }>`
+  max-height: ${({ shape }) =>
+    shape === LIST_ITEM_SHAPE.VIDEO ? "64px" : "56px"};
+  max-width: ${({ shape }) =>
+    shape === LIST_ITEM_SHAPE.VIDEO ? "114px" : "56px"};
+  overflow: hidden;
+`;
+const TrailingContent = styled.div`
+  max-width: 24px;
+`;
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  flex: 1;
+`;
+
+const Headline = styled.div`
+  color: var(--onSurface);
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 24px; /* 150% */
+  letter-spacing: 0.5px;
+`;
+const Children = styled.div`
+  color: var(--onSurfaceVariant);
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px; /* 142.857% */
+  letter-spacing: 0.25px;
+`;
+
+export const ListItem = forwardRef<HTMLLIElement, ListItemProps>(
+  (
+    {
+      children,
+      leading,
+      leadingWrapperProps,
+      headline,
+      headlineWrapperProps,
+      trailing,
+      trailingWrapperProps,
+      contentWrapperProps,
+      childrenWrapperProps,
+      bottomDivider = false,
+      shape = LIST_ITEM_SHAPE.DEFAULT,
+      onClick,
+      rippleEffect = true,
+      ...liProps
+    },
+    ref
+  ) => {
+    const { classNamePrefix } = useTheme();
+    const liRef = useRef<HTMLLIElement>();
+
+    useRipple(liRef, rippleEffect && !!onClick);
+
+    return (
+      <BaseListItem
+        ref={(el: HTMLLIElement) => {
+          if (ref) {
+            if (typeof ref === "function") {
+              ref(el);
+            } else {
+              ref.current = el;
+            }
+          }
+          liRef.current = el;
+        }}
+        classNamePrefix={classNamePrefix}
+        bottomDivider={bottomDivider}
+        shape={shape}
+        onClick={onClick}
+        {...liProps}
+      >
+        <LeadingContent {...leadingWrapperProps} shape={shape}>
+          {leading}
+        </LeadingContent>
+        <Content {...contentWrapperProps}>
+          <Headline {...headlineWrapperProps}>{headline}</Headline>
+          <Children {...childrenWrapperProps}>{children}</Children>
+        </Content>
+        <TrailingContent {...trailingWrapperProps}>{trailing}</TrailingContent>
+      </BaseListItem>
+    );
+  }
+);
